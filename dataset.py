8ee1b7d230bdc5bc3eb66f4ebcfe8e7860d14d72
PA-Segura <segurachavez@gmail.com>
2026-02-10 16:50:36 -0600

    borrado de dataset.py parece qeu fuue algo de dessarrollo..

--- dataset.py
+++
@@ -1,4 +0,0 @@
-# Falta la normalizacion
-imputed_files_folder = "/ZION/AirPollutionData/Data/MergedDataCSV/16/Imputed/bk1"
-# Falta la normalizacion
-INPUT_FOLDER = '/ZION/AirPollutionData/Data/MergedDataCSV/16/'
